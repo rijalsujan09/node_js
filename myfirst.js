a62588481ab84ec352d01941510c9b7c5c7dbb23
Sujan Rijal <rijalsujan@gmail.com>
2023-04-09 12:07:35 -0400

    soe changes

--- myfirst.js
+++ myfirst.js
@@ -2,6 +2,7 @@ var http  = require("http");
 var dt = require("./date");
 var url = require('url');
 var fs = require("fs");
+var uc = require('upper-case');
 
 // // creating a server object 
 // http.createServer(function(req, res){
@@ -75,8 +76,46 @@ var fs = require("fs");
 // })
 
 //--------------------------rename file---------------------------------
-//--->renaming file with  .rename() method
-fs.rename("demo1.html", "demo.html", function(err){
-  if(err) throw err;
-  console.log("renamed sucessfull...")
-})
+// //--->renaming file with  .rename() method
+// fs.rename("demo1.html", "demo.html", function(err){
+//   if(err) throw err;
+//   console.log("renamed sucessfull...")
+// })
+
+
+// ========================================================================================
+
+// ---> The Built-in URL Module 
+
+// var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+// var q = url.parse(adr, true);
+
+// console.log("local host => ",q.host); //returns 'localhost:8080'
+// console.log("path name => ",q.pathname); //returns '/default.htm'
+// console.log("search query => ",q.search); //returns '?year=2017&month=february'
+
+// var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+// console.log("search month => ",qdata.month); //returns 'february'
+
+
+// http.createServer(function (req, res) {
+//   var q = url.parse(req.url, true);
+//   var filename = "." + q.pathname;
+//   fs.readFile(filename, function(err, data) {
+//     if (err) {
+//       res.writeHead(404, {'Content-Type': 'text/html'});
+//       return res.end("404 Not Found");
+//     } 
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.write(data);
+//     return res.end();
+//   });
+// }).listen(8080);
+
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(uc.upperCase("Hello World!"));
+  res.end();
+}).listen(8080);
